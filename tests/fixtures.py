d3f10644fc06db64183c220943e9a6536860a837
jensfinnas <jens.finnas@gmail.com>
2017-02-22 14:04:41 +0100

    Add test for parent values

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -2,14 +2,30 @@
 
 import os 
 import pytest
+import csvkit as csv
 
-def iterate_files(directory):
+def iterate_files(directory, file_extension=None):
     """ Iterate all files in folder an subfolders
+
+        :param directory: Path to base dir to start iterating from
+        :param file_extension: "csv" or ".csv"
     """
+    if file_extension is not None:
+        if file_extension[0] is not ".":
+            file_extension = "." + file_extension
+
     for path, subdirs, files in os.walk(directory):
         for filename in files:
-            yield os.path.join(path, filename)
-         #print(str(f) + os.linesep) 
+            file_path = os.path.join(path, filename)
+
+            _name, _extension = os.path.splitext(file_path)
+            
+            if file_extension is None:
+                yield file_path
+            elif _extension == file_extension:
+                yield file_path
+            else:
+                continue
 
 
 @pytest.fixture(scope="session")
@@ -18,6 +34,21 @@ def get_datatype_files():
     """
     files = []
     for directory in ["misc","periods","regions"]:
-        files += [x for x in iterate_files(directory)]
+        file_iterator = iterate_files(directory, file_extension="csv")
+        files += [x for x in file_iterator]
 
     return files
+
+
+@pytest.fixture(scope="session")
+def get_ids(get_datatype_files):
+    """ Get a list of all ids in project
+    """
+    files = get_datatype_files
+    ids = []
+    for file_path in files:
+        with open(file_path) as f:
+            reader = csv.DictReader(f)
+            ids += [ x["id"] for x in reader ]
+
+    return ids
--- tests/test_datatype_files.py
+++ tests/test_datatype_files.py
@@ -1,21 +1,32 @@
 # encoding: utf-8
 
 
-from fixtures import get_datatype_files
+from fixtures import get_datatype_files, get_ids
 import csvkit as csv
 
 def get_duplicates(l):
     return set([x for x in l if l.count(x) > 1])
 
-def test_id_uniqueness(get_datatype_files):
+def test_id_uniqueness(get_ids):
     """ Test there are no id duplicates
     """
-    ids = []
-    for file_path in get_datatype_files:
-        with open(file_path) as f:
-            reader = csv.DictReader(f)
-            ids += [ x["id"] for x in reader ]
-
+    ids = get_ids
     duplicates = get_duplicates(ids)
     assert len(duplicates) == 0
+
+def test_parent_validity(get_datatype_files, get_ids):
+    """ Make sure that all values in parent columns are
+        valid indecies
+    """
+    files, ids = get_datatype_files, get_ids
+    for file_path in files:
+        with open(file_path) as f:
+            reader = csv.DictReader(f)
+            headers = reader.fieldnames
+            if "parent" in headers:
+                for row in reader:
+                    parent = row["parent"]
+                    if (parent is not None) and (parent != ''):
+                        assert parent in ids
+
 
