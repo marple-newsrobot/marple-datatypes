2891cc077e5aed1b26da3b76edeb8640959b7281
einarsandberg <einars92@gmail.com>
2017-06-26 16:51:44 +0200

    get all available datatypes

--- api/app.py
+++ api/app.py
@@ -8,10 +8,19 @@ import csvkit as csv
 import os
 from copy import deepcopy
 import pprint
+import sys
 pp = pprint.PrettyPrinter(indent=2)
 DEFAULT_LANG = "en"
 SCHEMA_DIR = "schemas"
+# Get from settings, should point to root folder
+DATATYPES_DIR = ".."
+# Get from request
+LANG = "en"
+# Get from request
+DOMAIN = "http://marple-datatypes.herokuapp.com"
 
+# Points to root folder
+RELATIONS_CSV_PATH = "../relations.csv"
 app = Flask(__name__, static_url_path = "")
 
 @app.errorhandler(400)
@@ -22,14 +31,20 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-@app.route("/datatypes", methods=['GET'])
+@app.route("/datatype", methods=['GET'])
 def get_datatypes():
-
-    
+    ALL_DOMAINS = Domain("*/*", datatypes_dir=DATATYPES_DIR)
+    data = []
+    file_path = os.path.join(DATATYPES_DIR, "datatypes.csv")
+    csv_file = CsvFile(file_path)
+    # TODO: Add path
+    return jsonify(csv_file.to_dictlist())
+    pp.pprint(csv_file.to_dictlist())
     return ""
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('DEBUG', False)
+    debug = os.environ.get('DEBUG', True)
     app.run(host='0.0.0.0', debug = debug, port=port)
+
