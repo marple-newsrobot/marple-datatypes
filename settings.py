e4119316122b12d60fc280a93cb6fa4d4d788b0a
einarsandberg <einars92@gmail.com>
2017-06-28 09:23:54 +0200

    heroku test

--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ import os
 #DATATYPES_DIR=os.environ.get('DATATYPES_DIR', "..")
 DATATYPES_DIR="./"
 DEFAULT_LANG=os.environ.get("DEFAULT_LANG", "en")
-RELATIONS_CSV_PATH = os.environ.get("RELATIONS_CSV_PATH", "../relations.csv")
+#RELATIONS_CSV_PATH = os.environ.get("RELATIONS_CSV_PATH", "../relations.csv")
+RELATIONS_CSV_PATH="./relations.csv"
