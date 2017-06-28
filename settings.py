3979002d8cfbb5c9d778628443a20b343c80626b
einarsandberg <einars92@gmail.com>
2017-06-28 09:09:55 +0200

    test heroku

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import os
-DATATYPES_DIR=os.environ.get('DATATYPES_DIR', "")
+DATATYPES_DIR=os.environ.get('DATATYPES_DIR', "marple-datatypes")
 DEFAULT_LANG=os.environ.get("DEFAULT_LANG", "en")
 RELATIONS_CSV_PATH = os.environ.get("RELATIONS_CSV_PATH", "../relations.csv")
